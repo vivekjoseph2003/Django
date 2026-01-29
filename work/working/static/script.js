92e487cb2953573fb11c8ccbeede835c3a0678eb
Vivek <vivekjoseph28@gmail.com>
2026-01-29 21:52:58 +0530

    classwork(day-2)

---
+++ work/working/static/script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    alert("Button clicked! Welcome to the site");
+}
---
+++ work/working/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+def home(request):
+    return render(request,'home.html')
+def about(request):
+    return render(request,'about.html')
